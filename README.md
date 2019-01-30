f2220224e857888c089e21ea6cf80910e2e452b8
Shota Shimazu <hornet.live.mf@gmail.com>
2019-01-30 22:13:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SimpleDefault
+
+SimpleDefault is UserDefault interface or manager written in Swift.
+
+# Features
+
+- Avoid conflicting key name.
+
+SimpleDefault automatically attach a prefix to each keys according to Manager’s class name. Thus, you don’t need to be careful about key name conflicts.
---
+++ SimpleDefaults/SimpleDefaults.h
@@ -0,0 +1,19 @@
+//
+//  SimpleDefaults.h
+//  SimpleDefaults
+//
+//  Created by Shota Shimazu on 2019/01/30.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleDefaults.
+FOUNDATION_EXPORT double SimpleDefaultsVersionNumber;
+
+//! Project version string for SimpleDefaults.
+FOUNDATION_EXPORT const unsigned char SimpleDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleDefaults/PublicHeader.h>
+
+
