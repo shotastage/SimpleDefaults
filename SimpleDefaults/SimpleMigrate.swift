dba8d19f05179cfef6aa4ec059685bb17f9c6957
Shota Shimazu <hornet.live.mf@gmail.com>
2019-02-13 18:29:08 +0900

    add migrator

---
+++ SimpleDefaults/SimpleMigrate.swift
@@ -0,0 +1,26 @@
+//
+//  SimpleMigrate.swift
+//  SimpleDefaults
+//
+//  Created by Shota Shimazu on 2019/02/13.
+//  Copyright © 2019 Shota Shimazu. All rights reserved.
+//
+
+import Foundation
+
+/// SimpleDefaults migrator
+open class SimpleMigrate {
+    
+    static func removeModelBy(className: String) {
+        
+        let modelClassInstance: AnyClass? = NSClassFromString(className)
+        
+        var properties: [String] {
+            return Mirror(reflecting: modelClassInstance.self!).children.compactMap{ $0.label }
+        }
+    }
+    
+    static func refreshStructureBy(className: String) {
+        
+    }
+}
